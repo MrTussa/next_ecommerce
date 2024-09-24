36150386e0a9a381fd251178e381f1d224654ce3
MrTussa <dd993736@gmail.com>
2024-09-24 20:49:11 +0500

    Added: product page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,7 +1,55 @@
+import Add from "@/components/Add";
+import CustomizeProducts from "@/components/CustomizeProducts";
+import ProductImages from "@/components/ProductImages";
 import React from "react";
 
 const SinglePage = () => {
-  return <div>SinglePage</div>;
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
+      {/* IMAGE */}
+      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+        <ProductImages />
+      </div>
+      {/* TEXT */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h2 className="text-4xl font-medium">Product Name</h2>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
+          ut.
+        </p>
+        <div className="h-[2px] bg-gray-100" />
+        <div className="flex items-center gap-4">
+          <h4 className="text-xl text-gray-500 line-through">50$</h4>
+          <h5 className="font-medium text-2xl">45$</h5>
+        </div>
+        <div className="h-[2px] bg-gray-100" />
+        <CustomizeProducts />
+        <Add />
+        <div className="h-[2px] bg-gray-100" />
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
+            quae.
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
+            quae.
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
+            quae.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePage;
